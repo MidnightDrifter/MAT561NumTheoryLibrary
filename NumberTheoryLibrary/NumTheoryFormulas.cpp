c20ec21b48749da005e64cf14aedd6bdaa2d92be
Owner <Owner@DESKTOP-DLMP2BP>
2016-09-15 18:26:08 -0700

    Core loops done, need to test

--- NumberTheoryLibrary/NumTheoryFormulas.cpp
+++ NumberTheoryLibrary/NumTheoryFormulas.cpp
@@ -14,6 +14,7 @@ std::vector<std::vector<long>> EuclideanAlgorithm(long a, long b)
 	long quotient = bigger / smaller;
 	long remainder = bigger % smaller;
 	
+	//output.push_back(std::vector<long>());
 	output.at(0).push_back(bigger);
 	output.at(0).push_back(quotient);
 	output.at(0).push_back(smaller);
@@ -36,7 +37,7 @@ std::vector<std::vector<long>> EuclideanAlgorithm(long a, long b)
 		quotient = bigger / smaller;
 		remainder = bigger % smaller;
 
-
+		output.push_back(std::vector<long>());
 		output.at(i).push_back(bigger);
 		output.at(i).push_back(quotient);
 		output.at(i).push_back(smaller);
@@ -47,16 +48,40 @@ std::vector<std::vector<long>> EuclideanAlgorithm(long a, long b)
 
 	}
 
-	//If we get here, either bigger OR smaller = 0
+	//If we get here, either bigger OR smaller = 0, GCD is the greater of the two
 	//Find the linear combination such that ax + by = GCD via looping waaaaay back through the matrix somehow, gonna be ugly af
 	//Push back that linear comb. + GCD
-	int a = 0;
-	int b = a;
 
-	for (auto i = output.end(); i != output.begin(); i--)
+	long gcd = (*output.end()).at(0);
+	if (gcd == 0)
 	{
+		gcd = (*output.end()).at(2);
+	}
+	
+	long curr[2] = { 1l,0l };
+	long next[2] = { 0l,1l };
+	long temp[2] = { 0l,0l };
+	for (auto i = output.begin(); (*i).at(3) !=0; i++)
+	{
+		temp[0] = next[0];
+		temp[1] = next[1];
+
+		next[0] -= (*i).at(1) * curr[0];
+		next[1] -= (*i).at(1) * curr[1];
+
+		curr[0] = temp[0];
+		curr[1] = temp[1];
+
+
 
 	}
+	
+	output.push_back(std::vector<long>());
+	output.at(output.size()).push_back(std::max(a, b));
+	output.at(output.size()).push_back(next[0]);
+	output.at(output.size()).push_back(std::min(a, b));
+	output.at(output.size()).push_back(next[1]);
+	output.at(output.size()).push_back(gcd);
 
 
 	return output;
